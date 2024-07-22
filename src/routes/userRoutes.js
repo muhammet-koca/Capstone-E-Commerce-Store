283dc47a9ff59432d93f042563281ed07b7e4524
Molly Morse <mollymorse@Mollys-Air.attlocal.net>
2024-07-22 09:19:39 -0500

    db fixed

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -22,7 +22,14 @@ route.post("/register", register); //tested
 route.post("/login", login); //tested
 route.get("/product/:id", getProductById); //get individual product(id) details tested
 
-//get cart
+//rerun prisma migrate
+//add item to cart (update cart)
+//update cartItems
+//set cart to null (item quantity zero?)
+//fix isAdmin
+//retest all routes
+//deploy backend
+
 route.get("/user/:id", authenticateToken, getUserById); //only get account details tested
 route.delete("/user/:id", authenticateToken, deleteUser); //only delete self tested
 route.put("/user/:id", authenticateToken, updateUser); //only update self tested
