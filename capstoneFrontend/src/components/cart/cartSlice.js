950065545539e8f0df06f3c3e20d27c44b14a9a7
Yassin <yassin1999@gmail.com>
2024-07-26 14:06:43 -0400

    fixed checkout backend

--- capstoneFrontend/src/components/cart/cartSlice.js
+++ capstoneFrontend/src/components/cart/cartSlice.js
@@ -14,7 +14,7 @@ const getCartApi = api.injectEndpoints({
     deleteCart: builder.mutation({
       query: ({ id }) => ({
         url: `/store/checkout/${id}`,
-        method: "DELETE",
+        method: "PUT",
         responseHandler: (response) => response.text(),
       }),
       invalidatesTags: ["User"],
