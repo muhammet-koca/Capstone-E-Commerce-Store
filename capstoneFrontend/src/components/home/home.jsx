229b1c3ab693ab058b3a8b6729fc5eb38a5818cc
MuhammetKoca2810 <muhammetkoca2810@gmail.com>
2024-07-30 11:23:25 -0500

    sort added

--- capstoneFrontend/src/components/home/home.jsx
+++ capstoneFrontend/src/components/home/home.jsx
@@ -7,6 +7,7 @@ import "./home.css";
 import { useSelector } from "react-redux";
 
 export default function Home() {
+  const [sortOption, setSortOption] = useState("name-asc");
   const { data: products = [], isSuccess, isLoading } = useGetProductQuery();
 
   useEffect(() => {
@@ -22,24 +23,51 @@ export default function Home() {
     );
   }
 
+  const handleSortChange = (event) => {
+    setSortOption(event.target.value);
+  };
+
+  const sortedProducts = [...products].sort((a, b) => {
+    if (sortOption.startsWith("price")) {
+      const priceComparison = a.price - b.price;
+      return sortOption === "price-asc" ? priceComparison : -priceComparison;
+    } else if (sortOption.startsWith("name")) {
+      const nameComparison = a.productName.localeCompare(b.productName);
+      return sortOption === "name-asc" ? nameComparison : -nameComparison;
+    }
+    return 0;
+  });
+
   return (
-    <div className="product-list">
-      {isSuccess &&
-        products.map((product) => (
-          <div className="product-card" key={product.id}>
-            <Link to={`/product/${product.id}`} className="product-link">
-              <img
-                src={product.image}
-                alt={product.productName}
-                className="product-image"
-              />
-              <h2 className="product-name">{product.productName}</h2>
-              <p className="product-price">
-                Price: ${product.price.toFixed(2)}
-              </p>
-            </Link>
-          </div>
-        ))}
+    <div>
+      <div className="sort-options">
+        <label htmlFor="sort-select">Sort by:</label>
+        <select id="sort-select" value={sortOption} onChange={handleSortChange}>
+          <option value="name-asc">Product Name (A to Z)</option>
+          <option value="name-desc">Product Name (Z to A)</option>
+          <option value="price-asc">Price (Low to High)</option>
+          <option value="price-desc">Price (High to Low)</option>
+        </select>
+      </div>
+
+      <div className="product-list">
+        {isSuccess &&
+          sortedProducts.map((product) => (
+            <div className="product-card" key={product.id}>
+              <Link to={`/product/${product.id}`} className="product-link">
+                <img
+                  src={product.image}
+                  alt={product.productName}
+                  className="product-image"
+                />
+                <h2 className="product-name">{product.productName}</h2>
+                <p className="product-price">
+                  Price: ${product.price.toFixed(2)}
+                </p>
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
